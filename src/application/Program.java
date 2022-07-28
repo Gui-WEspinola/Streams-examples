eb839ee7767b8dc98633ebb0f7fc18dd22b37e7b
Guilherme Espinola <gwanderleyespinola@gmail.com>
2022-07-27 21:03:18 -0300

    More streams examples

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,6 +9,9 @@ public class Program {
 
         List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
 
+        Stream<Integer> st = list.stream().map(x -> x * 10); //operação intermediária
+        System.out.println(Arrays.toString(st.toArray())); //operação terminal
+
         Stream<Integer> st1 = list.stream().map(x -> x * 10);
         System.out.println(Arrays.toString(st1.toArray()));
 
@@ -20,5 +23,13 @@ public class Program {
 
         Stream<Long> st4 = Stream.iterate(new Long[]{0L, 1L}, p -> new Long[] {p[1], p[0]+p[1]}).map(p -> p[0]);
         System.out.println(Arrays.toString(st4.limit(25).toArray()));
+
+        int sum = list.stream().reduce(0, (x, y) -> x + y);
+        System.out.println("Sum = " + sum);
+
+        List<Integer> newList = list.stream()
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * 10).toList();
+        System.out.println(Arrays.toString(newList.toArray()));
     }
 }
